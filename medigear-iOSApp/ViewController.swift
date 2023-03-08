f90d32af11839f3d49a267e73a58ea6ae078d883
Gabo748 <gaeducampos@gmail.com>
2023-03-08 15:14:10 -0600

    Initial Commit

---
+++ medigear-iOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  medigear-iOSApp
+//
+//  Created by Gabriel Campos on 8/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
